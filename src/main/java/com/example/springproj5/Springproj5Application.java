472f0b0f7df9810f05179058cc33347617527e27
jeka-01 <zeka.abbasov.bhos@gmail.com>
2021-12-11 20:44:27 +0400

    project x

--- src/main/java/com/example/springproj5/Springproj5Application.java
+++ src/main/java/com/example/springproj5/Springproj5Application.java
@@ -1,13 +1,13 @@
-package com.example.springproj4;
+package com.example.springproj5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springproj4Application {
+public class Springproj5Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Springproj4Application.class, args);
+        SpringApplication.run(Springproj5Application.class, args);
     }
 
 }
