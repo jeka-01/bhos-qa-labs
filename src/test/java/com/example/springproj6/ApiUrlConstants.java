16ef554274fd155acfaa80f921073097e4344697
jeka-01 <63017516+jeka-01@users.noreply.github.com>
2021-12-12 15:16:39 +0400

    Update ApiUrlConstants.java

--- src/test/java/com/example/springproj6/ApiUrlConstants.java
+++ src/test/java/com/example/springproj6/ApiUrlConstants.java
@@ -3,7 +3,7 @@ package com.example.springproj6;
 public class ApiUrlConstants {
 
     public final static String FIREBASE_SIGNIN_EMAIL_AND_PASSWORD = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=";
-    public final static String FIRESTORE_DOCUMENT = "https://firestore.googleapis.com/v1/projects/fb-demo-c6774/databases/(default)/documents/users";
-    public final static String FIREBASE_STORAGE_AVATAR = "https://firebasestorage.googleapis.com/v0/b/fb-demo-c6774.appspot.com/o";
+    public final static String FIRESTORE_DOCUMENT = "https://firestore.googleapis.com/v1/projects/architecture-d8267/databases/(default)/documents/users";
+    public final static String FIREBASE_STORAGE_AVATAR = "https://firebasestorage.googleapis.com/v0/b/architecture-d8267.appspot.com/o";
 
 }
