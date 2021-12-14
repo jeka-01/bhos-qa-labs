07c04cdb35d27735780ea9257eaa7266b06d1462
jeka-01 <63017516+jeka-01@users.noreply.github.com>
2021-12-14 09:25:27 +0300

    Update ApiUrlConstants.java

--- src/test/java/com/example/springproj5/ApiUrlConstants.java
+++ src/test/java/com/example/springproj5/ApiUrlConstants.java
@@ -3,6 +3,6 @@ package com.example.springproj5;
 public class ApiUrlConstants {
 
     public final static String FIREBASE_SIGNIN_EMAIL_AND_PASSWORD = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=";
-    public final static String FIRESTORE_USERS = "https://firestore.googleapis.com/v1/projects/bhos-qa-labs-18574/databases/(default)/documents/users/";
+    public final static String FIRESTORE_USERS = "https://firestore.googleapis.com/v1/projects/architecture-d8267/databases/(default)/documents/users/";
     public final static String FIREBASE_API_KEY = "<REDACTED>"; // GITHUB SECRET
 }
