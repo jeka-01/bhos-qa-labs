f3a321d59b7cc5225be17889ec1e23679cc15a53
jeka-01 <zeka.abbasov.bhos@gmail.com>
2021-12-15 13:19:46 +0400

    hello there

--- src/main/java/com/example/springproj7/Springproj7Application.java
+++ src/main/java/com/example/springproj7/Springproj7Application.java
@@ -1,13 +1,13 @@
-package com.example.springproj5;
+package com.example.springproj7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springproj5Application {
+public class Springproj7Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Springproj5Application.class, args);
+        SpringApplication.run(Springproj7Application.class, args);
     }
 
 }
